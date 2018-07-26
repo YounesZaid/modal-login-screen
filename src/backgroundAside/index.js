1abb7b676a395d4479d071081719425b71d59493
Younes Zaid <zaidyounes94@gmail.com>
2018-07-26 17:56:22 +0100

    Structured project

---
+++ src/backgroundAside/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+
+const BackgroundAside = () => (
+  <div className="background-container">
+    <h1>BackgroundAside here</h1>
+  </div>
+)
+
+export default BackgroundAside;
---
+++ src/loginAside/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './index.css';
+
+class LoginAside extends Component {
+  render() {
+    return (
+      <div className="login-container">
+        <h1>Login Aside</h1>
+      </div>
+    )
+  }
+}
+
+export default LoginAside;
