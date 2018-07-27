f87c5cbd9873b8f59f6835cc99ac14f7193719ee
Younes Zaid <zaidyounes94@gmail.com>
2018-07-27 19:52:14 +0100

    Added auth ui section

--- src/loginAside/index.js
+++ src/loginAside/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from "@blueprintjs/core";
+import { Checkbox } from "@blueprintjs/core";
 
 import './index.css';
 
@@ -13,18 +13,34 @@ class LoginAside extends Component {
 
   render() {
     return (
-      <div className="login-container">
-        <div className="bp3-input-group bp3-intent-primary">
-          <span className="bp3-icon bp3-icon-lock"></span>
-          <input type="email" className="bp3-input" placeholder="Filter histogram..." />
+      <div className="authentification-container">
+        <div className="login-section">
+          <div className="bp3-input-group email-input">
+            <i className="zmdi zmdi-account-circle zmdi-hc-2x"></i>
+            <input type="text" className="bp3-input bp3-large bp3-fill" placeholder="Username or email" />
+          </div>
+          <div className="bp3-input-group password-input">
+            <i className="zmdi zmdi-key zmdi-hc-2x"></i>
+            <input type="password" className="bp3-input bp3-large bp3-fill" placeholder="Password" />
+          </div>
         </div>
-        <div className="bp3-input-group .modifier">
-          <span className="bp3-icon bp3-icon-search"></span>
-          <input type="text" className="bp3-input" placeholder="Search" />
-          <button className="bp3-button bp3-minimal bp3-intent-primary bp3-icon-arrow-right" ></button>
+        <div className="buttons-section">
+          <Checkbox checked={this.state.isEnabled} label="Remember me" onChange={this.handleEnabledChange} />
+          {/* <Button>
+            LOGIN
+          </Button> */}
+          <button className="login-button" onClick={() => {
+            alert("clicked");
+          }}> LOGIN </button>
         </div>
+        <div className="register-reset-section">
+          <a onClick={() => {
 
-        <Button intent="primary" text="button content" onClick={() => { alert("clicked") }} />
+          }} >Register Now</a>
+          <a onClick={() => {
+            
+          }} >Forgot Password?</a>
+        </div>
       </div>
     )
   }
