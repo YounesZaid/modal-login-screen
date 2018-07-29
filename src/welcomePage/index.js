8621f432f1306bb7ae685aa09c08fa645d9d58dd
Younes Zaid <zaidyounes94@gmail.com>
2018-07-29 21:00:01 +0100

    Edited ui

--- src/welcomePage/index.js
+++ src/welcomePage/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './index.css';
 
-const WelcomePage = () => (
+const WelcomePage = (props) => (
   <div className='mls-welcome'>
-    <h1>Welcome to the main Page, Happy Signin!</h1>
+    <div className='mls-welcome-content'>
+      <h1>Welcome to the main Page, Happy Signin!</h1>
+      <div className='mls-welcome-infos'>
+        <p>
+          Username : <span> Younes Zaid</span>
+        </p>
+        <p>
+          Password : <span> Password </span>
+        </p>
+      </div>
+      <button onClick={() => {
+        props.history.push('/login');
+      }}>
+        Go Out
+      </button>
+    </div>
   </div>
 )
 
-export default WelcomePage;
+export default withRouter(WelcomePage);
